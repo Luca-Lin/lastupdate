75f9864fc53f35fef81fdfc10edc5aff05ece1e0
ZongYing Lin <56711360+Luca-Lin@users.noreply.github.com>
2024-07-28 16:37:31 +0800

    Update install.sh

--- macOS/install/install.sh
+++ macOS/install/install.sh
@@ -95,7 +95,7 @@ install-dev-software() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
     source ~/.nvm/nvm.sh
 
